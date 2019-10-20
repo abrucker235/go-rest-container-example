1f550b9ae280354ffe20807ce6e4bc05100608be
aaron <aaron.brucker@gmail.com>
2019-10-20 08:39:42 -0400

    Adding server main startup

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var server *http.Server
+
+func main() {
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Failed to start server.")
+		}
+	}()
+	log.Println("Server up and listening.")
+
+	<-done
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failure:%s\n", err)
+	}
+	log.Println("Server shutdown properly.")
+}
